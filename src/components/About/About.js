69709645d861e2dbffc93a947abe2be0623939e9
Piotr Pelc <piotrpelc83@gmail.com>
2025-02-24 00:11:54 +0100

    Add React Router and implement subpage navigation

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import styles from './About.module.scss';
+import Container from '../Container/Container';
+import PageTitle from '../PageTitle/PageTitle';
+
+const About = () => {
+  return(
+    <Container>
+      <PageTitle>about</PageTitle>
+      <p>lorem ipsum</p>
+    </Container>
+  );
+};
+
+export default About;
---
+++ src/components/Favorite/Favorite.js
@@ -0,0 +1,14 @@
+import styles from './Favorite.module.scss';
+import Container from '../Container/Container';
+import PageTitle from '../PageTitle/PageTitle';
+
+const Favorite = () => {
+  return(
+    <Container>
+      <PageTitle>favorite</PageTitle>
+      <p>lorem ipsum</p>
+    </Container>
+  );
+};
+
+export default Favorite;
